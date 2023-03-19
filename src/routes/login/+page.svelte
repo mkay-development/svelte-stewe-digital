fff747d688b93dd6688091f4674125f54ccafb08
Martz <github@jmartz.de>
2023-03-19 18:03:31 +0100

    add button style login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,7 +1,3 @@
-<svelte:head>
-  <title>Login - Stewe Digital</title>
-</svelte:head>
-
 <script>
   import { goto } from "$app/navigation";
   import PocketBase from "pocketbase";
@@ -13,15 +9,17 @@
   const login = async function () {
     const pb = new PocketBase($url);
 
-    await pb
-      .collection("users")
-      .authWithPassword(email, password);
+    await pb.collection("users").authWithPassword(email, password);
     if (pb.authStore.isValid) {
       goto("/dashboard");
       window.location = "/dashboard";
     }
   };
 </script>
+
+<svelte:head>
+  <title>Login - Stewe Digital</title>
+</svelte:head>
 
 <div class="grid grid-cols-6 gap-3">
   <div class="col-span-6 md:col-span-2">
@@ -52,10 +50,22 @@
         />
       </div>
       <section class="actions text-right">
-        <button type="submit">login</button>
+        <button
+          type="submit"
+          class="px-2 py-2 bg-gray-400 mt-3 text-white font-bold">login</button
+        >
       </section>
     </form>
   </div>
-  <div class="col-span-6 md:col-span-2">Test</div>
+  <div class="col-span-6 md:col-span-2">
+    <h2 class="font-bold text-lg mx-2 mb-3">Demo Login Daten:</h2>
+    <section class="card bg-white px-2 py-2 rounded-lg">
+      <span class="label font-bold block">Email</span>
+      <span class="value block">admin@jmartz.de</span>
+      <br />
+      <span class="label font-bold block">Password</span>
+      <span class="value block">findetnemo</span>
+    </section>
+  </div>
   <div class="col-span-6 md:col-span-2">Test</div>
 </div>
